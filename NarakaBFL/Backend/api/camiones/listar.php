0eb483b7bcac6aea84d71235c6b560ab78d6443f
fabrii <fabriciobanquero9@gmail.com>
2026-07-20 13:02:02 -0300

    Update listar.php

--- NarakaBFL/Backend/api/camiones/listar.php
+++ NarakaBFL/Backend/api/camiones/listar.php
@@ -1,5 +1,4 @@
 <?php
-// listar.php
 require_once __DIR__ . '/../../config/session.php';
 require_once __DIR__ . '/../../helpers/response.php';
 
@@ -7,4 +6,4 @@ if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
     responder(405, null, 'Método no permitido');
 }
 
-responder(200, $_SESSION['contenedores'], 'Listado de contenedores');
+responder(200, $_SESSION['camiones'], 'Listado de camiones');
